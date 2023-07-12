756531dfba2999852d9a76dc26cf46d930a96d84
Pedro <pedrodias3737@gmail.com>
2023-07-12 09:31:17 -0300

    Project created

---
+++ src/test/java/phenriqued/com/github/gameList/GameListApplicationTests.java
@@ -0,0 +1,13 @@
+package phenriqued.com.github.gameList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

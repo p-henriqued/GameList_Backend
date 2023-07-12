0ec0cd20b8ba2b54052ec490800992cf26f32742
Pedro <pedrodias3737@gmail.com>
2023-07-12 16:11:52 -0300

    domain model implementation.

---
+++ src/main/java/phenriqued/com/github/gameList/entities/BelongingPK.java
@@ -0,0 +1,55 @@
+package phenriqued.com.github.gameList.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPK {
+	
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	Game game;
+	@ManyToOne
+	@JoinColumn(name = "list_id")
+	GameList list;
+	
+	public BelongingPK() {}
+	
+	public BelongingPK(Game game, GameList gameList) {
+		this.game = game;
+		this.list = gameList;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+	public void setGame(Game game) {
+		this.game = game;
+	}
+	public GameList getList() {
+		return list;
+	}
+	public void setList(GameList gameList) {
+		this.list = gameList;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, list);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPK other = (BelongingPK) obj;
+		return Objects.equals(game, other.game) && Objects.equals(list, other.list);
+	}
+	
+}
--- src/main/java/phenriqued/com/github/gameList/entities/GameList.java
+++ src/main/java/phenriqued/com/github/gameList/entities/GameList.java
@@ -2,8 +2,18 @@ package phenriqued.com.github.gameList.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game_list")
 public class GameList {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
